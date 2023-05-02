852c0f254a6a8d9b20134ab8158c511eb1601539
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-02 14:57:25 -0700

    rebuilt UI using grid

--- image_converter.py
+++ image_converter.py
@@ -2,24 +2,11 @@ import os
 import cv2 as cv
 from image_functions import *
 import tkinter as tk
+from tkinter import ttk
+from tkinter import * #type: ignore
 from tkinter import filedialog as fd
 
-# build window
-window = tk.Tk()
-window.title('Image Converter')
-window.resizable(False, False)
-window.geometry('700x365')
-
-file_scroll = tk.Scrollbar(window)
-file_box = tk.Listbox(window, yscrollcommand= file_scroll.set, height=15, width=30)
-file_box.place(x='20', y='50')
-file_scroll.config(command=file_box.yview)
-
-# input label
-input_label = tk.Label(window, text='Input', anchor='center')
-input_label.place(x='20', y='15')
-
-# file browse and open and save
+# functions
 def openfile():
     global images
     images = fd.askopenfilenames()
@@ -28,39 +15,13 @@ def openfile():
         name_l = name.rfind('/')
         img_name = name[name_l+1:]
         file_box.insert('end', img_name)
-images = ()
-open_button = tk.Button(window, text='Open', command=openfile)
-open_button.place(x='20', y='315')
-
-# output label
-output_label = tk.Label(window, text='Output', anchor='center')
-output_label.place(x='380', y='15')
-
-# options buttons
-var = tk.StringVar()
-gray_option = tk.Radiobutton(window, text='Grayscale', variable=var, value='grayscale')
-gray_option.place(x='380', y='50')
-sepia_option = tk.Radiobutton(window, text='Sepia', variable=var, value='sepia')
-sepia_option.place(x='380', y='80')
-mirror_option = tk.Radiobutton(window, text='Mirror', variable=var, value='mirror')
-mirror_option.place(x='380', y='110')
-reflect_option = tk.Radiobutton(window, text='Reflect', variable=var, value='reflect')
-reflect_option.place(x='380', y='140')
-scale_option = tk.Radiobutton(window, text='Scale', variable=var, value='scale')
-scale_option.place(x='380', y='170')
-
-s = tk.IntVar()
-half_option = tk.Radiobutton(window, text='.5x', variable=s, value=1)
-half_option.place(x='400',y='200')
-double_option = tk.Radiobutton(window, text='2x', variable=s, value=2)
-double_option.place(x='400',y='230')
 
 def new_factor():
     if s.get() == 1:
         factor = 2.0
     if s.get() == 2:
         factor = 0.5
-    return factor
+    return factor #type: ignore
 
 def process():
     path = fd.askdirectory()
@@ -89,8 +50,53 @@ def process():
             new_name = f"sc_{img_name}"
             cv.imwrite(os.path.join(f"{path}/", new_name), scale(img, h , w, new_factor()))
 
+images = ()
+
+# build window
+window = Tk()
+window.title('Image Converter')
+window.resizable(False, False)
+window.columnconfigure(0, weight=1)
+window.rowconfigure(0, weight=1)
+frame = ttk.Frame(window, padding='12 12 12 12')
+frame.grid(column=0, row=0, sticky=(N, W, E, S)) # type: ignore
+
+
+# input
+input_label = ttk.Label(frame, text='Input')
+input_label.grid(column=1, row=1, sticky=W)
+file_scroll = ttk.Scrollbar(frame)
+file_box = tk.Listbox(frame, yscrollcommand= file_scroll.set)
+file_box.grid(column=1, row=2, columnspan=30, rowspan=15, sticky=W)
+file_scroll.config(command=file_box.yview)
+open_button = ttk.Button(frame, text='Open', command=openfile)
+open_button.grid(column=1, row=17, sticky=S)
+
+
+# output
+output_label = ttk.Label(frame, text='Output')
+output_label.grid(column=32, row=1, sticky=W)
+
+# options buttons
+var = StringVar()
+gray_option = ttk.Radiobutton(frame, text='Grayscale', variable=var, value='grayscale')
+gray_option.grid(column=32, row=2, sticky=W)
+sepia_option = ttk.Radiobutton(frame, text='Sepia', variable=var, value='sepia')
+sepia_option.grid(column=32, row=3, sticky=W)
+mirror_option = ttk.Radiobutton(frame, text='Mirror', variable=var, value='mirror')
+mirror_option.grid(column=32, row=4, sticky=W)
+reflect_option = ttk.Radiobutton(frame, text='Reflect', variable=var, value='reflect')
+reflect_option.grid(column=32, row=5, sticky=W)
+scale_option = ttk.Radiobutton(frame, text='Scale', variable=var, value='scale')
+scale_option.grid(column=32, row=6, sticky=W)
+s = IntVar()
+half_option = ttk.Radiobutton(frame, text='.5x', variable=s, value=1)
+half_option.grid(column=32, row=7, sticky=N)
+double_option = ttk.Radiobutton(frame, text='2x', variable=s, value=2)
+double_option.grid(column=32, row=8, sticky=N)
+
 # conversion action button
-process_button = tk.Button(text='PROCESS', command=process)
-process_button.place(x='440', y='315')
+process_button = ttk.Button(frame, text='PROCESS', command=process)
+process_button.grid(column=32, row=17, sticky=W)
 
 window.mainloop()
